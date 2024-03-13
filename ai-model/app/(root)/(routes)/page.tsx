5add2945717823174447dee537b4cbb26e72e320
Vijay <vijaykumariitjee1092002@gmail.com>
2024-03-14 01:38:59 +0530

    First_commit

---
+++ ai-model/app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { SearchInput } from "@/components/search-input";
+
+
+const Rootpage = () => {
+    return ( 
+        <div className="h-full p-4 space-y-2">
+          <SearchInput />
+        </div>
+     );
+}
+ 
+export default Rootpage;
---
+++ ai-model/components/search-input.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { Search } from "lucide-react";
+
+import { Input } from "./ui/input";
+import { useState } from "react";
+
+export const SearchInput = () =>
+{ 
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+   const categoryId = searchParams.get("categoryId");
+   const name = searchParams.get("name");
+
+   const [value, setValue] = useState(name || "");
+  
+    return (
+        <div className="relative">
+            <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground"/>
+            <Input
+             placeholder="Search..." className="pl-10 bg-primary/10"
+            />
+        </div>
+     
+    )
+    
+}
