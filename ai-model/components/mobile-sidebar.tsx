14db9d5e86e134be352d1fd23e56ae4edeffe780
Vijay <vijaykumariitjee1092002@gmail.com>
2024-04-07 02:07:19 +0530

    some fixes

--- ai-model/components/mobile-sidebar.tsx
+++ ai-model/components/mobile-sidebar.tsx
@@ -8,14 +8,18 @@ import{
 
 import {Sidebar} from "./Sidebar";
 
-export const MobileSidebar = () => {
+export const MobileSidebar = ({
+    isPro
+}:{
+    isPro:boolean;
+}) => {
     return (
         <Sheet>
          <SheetTrigger className = "md:hidden pr-4" >
             <Menu />
          </SheetTrigger>
          <SheetContent className = "p-0 bg-secondary pt-10 w-32" side="left">
-              <Sidebar />
+              <Sidebar isPro={isPro} />
          </SheetContent>
         </Sheet>
       
